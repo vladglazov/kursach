cf946939b7cc2cfdf6c3146f73be26e0bf2d767b
vladglazov <vlad98glazov@gmail.com>
2021-05-27 10:18:13 +0300

    Update degrade_data.py

--- degrade_data.py
+++ degrade_data.py
@@ -2,12 +2,12 @@ import numpy as np
 import pydub
 # apt-get install libav-tools libavcodec-extra
 
-def create_good_bad_sound(filename: str):
-    soundGood = pydub.AudioSegment.from_mp3(filename)
-    soundGood = soundGood.set_channels(1).set_frame_rate(5000)
-    soundBad = soundGood
-    soundBad = soundBad.set_frame_rate(4900)
-    return (soundGood, soundBad)
+def create_high_low_sound(filename: str):
+    soundHigh = pydub.AudioSegment.from_mp3(filename)
+    soundHigh = soundHigh.set_channels(1).set_frame_rate(5000)
+    soundLow = soundHigh
+    soundLow = soundLow.set_frame_rate(4900)
+    return (soundHigh, soundLow)
 
 def generator_10sec_song(sound: pydub.AudioSegment, duration: int = 100):
     while len(sound) > duration:
@@ -15,17 +15,17 @@ def generator_10sec_song(sound: pydub.AudioSegment, duration: int = 100):
         sound = sound[duration:]
     yield sound
 
-def generator_bad_and_good_sound(mp3):
-    (soundGood, soundBad) = create_good_bad_sound(mp3)
-    good_gen = generator_10sec_song(soundGood)
-    bad_gen = generator_10sec_song(soundBad)
-    for g, b in zip(good_gen, bad_gen):
-        yield(([[x / 255. for x in b.raw_data]]), ([[y / 255. for y in g.raw_data]]))
+def generator_low_and_high_sound(mp3):
+    (soundHigh, soundLow) = create_high_low_sound(mp3)
+    high_gen = generator_10sec_song(soundHigh)
+    low_gen = generator_10sec_song(soundLow)
+    for g, b in zip(high_gen, low_gen):
+        yield(([[x / 255. for x in l.raw_data]]), ([[y / 255. for y in h.raw_data]]))
 
 if __name__ == '__main__':
     import download_data
     for path in download_data.getFileIterator():
-        for a in generator_bad_and_good_sound(path):
+        for a in generator_low_and_high_sound(path):
             print('len(sound[0]):', len(a[0]))
             print('len(sound[1]):', len(a[1]))
             break
